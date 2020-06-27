4e202f57d995b9bafc5e1bf52cca769585b817c4
codeaswell <jeremyaldesign@gmail.com>
2020-06-27 10:21:36 +0200

    add pages

--- src/Controller/ChronosController.php
+++ src/Controller/ChronosController.php
@@ -8,12 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChronosController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function index()
     {
         return $this->render('pages/show.html.twig', [
             'title' => 'Chronos'
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login()
+    {
+        return $this->render('pages/login.html.twig', [
+            'title' => 'Veuillez vous authentifier'
         ]);
     }
 }
